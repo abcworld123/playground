50f170c545c96b47138c4f2ed31608db8580b3cd
abcworld123 <rkaxhdals@gmail.com>
2022-03-28 00:35:28 +0900

    Add weather api get (3)

--- routes/games/weather.js
+++ routes/games/weather.js
@@ -6,6 +6,7 @@ router.get('/', (req, res, next) => {
   res.render('games/weather');
 });
 
+/* 오늘 날씨 맞히기 */
 router.post('/getWeatherDay', (req, res, next) => {
   let sec = (new Date().getTime() + 25200000);
   sec += -sec % 10800000 + 7200000;
@@ -41,7 +42,9 @@ router.post('/getWeatherDay', (req, res, next) => {
   });
 });
 
-router.post('/getWeatherWeek', (req, res, next) => {
+
+/* 주간 날씨 맞히기 (강수 확률) */
+router.post('/getWeatherTa', (req, res, next) => {
   let sec = (new Date().getTime() + 10800000);
   sec += -sec % 43200000 + 21600000;
   const now = new Date(sec).toISOString().replace(/[T\-:]|\..+/g, '').substring(0, 12);
@@ -66,6 +69,41 @@ router.post('/getWeatherWeek', (req, res, next) => {
     for (let i = 3; i < 10; i++) {
       arr.push(data[`${taTarget}${i}`]);
     }
+    res.send(arr);
+  });
+});
+
+
+// 주간 날씨 맞히기 (강수 확률)
+router.post('/getWeatherMl', (req, res, next) => {
+  let sec = (new Date().getTime() + 10800000);
+  sec += -sec % 43200000 + 21600000;
+  const now = new Date(sec).toISOString().replace(/[T\-:]|\..+/g, '').substring(0, 12);
+  const regId = ['', '11B00000', '11H20000', '11H10000', '11B00000', '11C20000', '11F20000', '11H20000'];
+  const regIdx = req.body.reg;
+  let arr = [];
+
+  request.get({
+    url: 'http://apis.data.go.kr/1360000/MidFcstInfoService/getMidLandFcst',
+    qs: {
+      serviceKey: process.env.WEATHER_KEY,
+      dataType: 'json',
+      regId: regId[regIdx],
+      tmFc: now
+    },
+    json: true
+  }, (error, response, body) => {
+    if (error) res.status(500).send('getWeather: error');
+    const data = body.response.body.items.item[0];
+    arr = [
+      data['rnSt3Pm'],
+      data['rnSt4Pm'],
+      data['rnSt5Pm'],
+      data['rnSt6Pm'],
+      data['rnSt7Pm'],
+      data['rnSt8'],
+      data['rnSt9'],
+    ];
     res.send(arr);
   });
 });
