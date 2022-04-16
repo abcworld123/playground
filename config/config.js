4d766e6d60564a790b1d18d89f651b0afc1e5893
KimDeokjung <ejrwnd123@gmail.com>
2022-04-16 21:26:21 +0900

    change indent 4 to 2

--- config/config.js
+++ config/config.js
@@ -2,13 +2,13 @@ const path = require('path')
 require('dotenv').config({path:path.join('.','.env')});
 
 module.exports = {
-    youtube: {
-        key: process.env.YOUTUBE_DATA_API_V3_KEY
-    },
-    database:{
-        mongooseAUTH: process.env.MONGOOSE_AUTH,
-        mongooseID: process.env.MONGOOSE_ID,
-        mongoosePW: process.env.MONGOOSE_PW,
-        serverURL: process.env.DATABASE_URL
-    },
+  youtube: {
+    key: process.env.YOUTUBE_DATA_API_V3_KEY
+  },
+  database:{
+    mongooseAUTH: process.env.MONGOOSE_AUTH,
+    mongooseID: process.env.MONGOOSE_ID,
+    mongoosePW: process.env.MONGOOSE_PW,
+    serverURL: process.env.DATABASE_URL
+  },
 }
