7a16036d3244a3d9be395d209f7e1f7cad96a6c9
abcworld123 <rkaxhdals@gmail.com>
2022-04-26 21:35:40 +0900

    Convert js to typescript

---
+++ public/javascripts/random/random.ts
@@ -0,0 +1,17 @@
+type p = HTMLParagraphElement;
+type input = HTMLInputElement;
+
+function start() {
+  const range = <input>document.getElementById('range');
+  const randint = <p>document.getElementById('randint');
+  const n = range.value;
+  if (!n) alertError();
+  else randint.innerText = String(Math.floor(Math.random() * (parseInt(n) + 1)));
+}
+
+function alertError() {
+  return Swal.fire({
+    icon: 'error',
+    title: 'N을 입력해주세요.',
+  });
+}
---
+++ public/javascripts/weather/weather.ts
@@ -0,0 +1,131 @@
+type div = HTMLDivElement;
+type span = HTMLSpanElement;
+type button = HTMLButtonElement;
+type input = HTMLInputElement;
+
+const userNums = [0, 2, 2, 2, 2, 2, 2, 2];
+const regNums = [0, 0, 0, 0, 0, 0, 0, 0];
+const regNames = ['서울', '부산', '대구', '인천', '대전', '광주', '울산'];
+
+const tab = document.querySelectorAll<div>('.tab-game');
+const userContainer = document.querySelectorAll<div>('.user-container');
+
+function alertError(text: string) {
+  return Swal.fire({
+    icon: 'error',
+    title: text,
+  });
+}
+
+function userPlus(i: number) {
+  const idx = i - 1;
+  const userList = userContainer[idx].children;
+  const newUser = <div>userList[0].cloneNode(true);
+  newUser.removeAttribute('style');
+  tab[idx].querySelector<span>('.user-num').innerText = String(++userNums[i]);
+  userContainer[idx].insertBefore(newUser, userList[userNums[i]]);
+  if (userNums[i] === 2) {
+    tab[idx].querySelector<button>('.btn-user-minus').disabled = false;
+  }
+}
+
+function userMinus(i: number) {
+  const idx = i - 1;
+  tab[idx].querySelector<span>('.user-num').innerText = String(--userNums[i]);
+  tab[idx].querySelector<div>('.user-container').children[userNums[i] + 1].remove();
+  if (userNums[i] === 1) {
+    tab[idx].querySelector<button>('.btn-user-minus').disabled = true;
+  }
+}
+
+function regSet(i: number, n: number) {
+  regNums[i] = n;
+  const btnText = tab[i - 1].querySelector<span>('.btn-text');
+  btnText.innerText = regNames[n - 1];
+}
+
+function start(i: number) {
+  let url: string, formdata: Record<string, any>;
+  const idx = i - 1;
+  const btnStart = tab[idx].querySelector<button>('.btn-start');
+
+  if (i <= 4) {
+    const nx = parseInt(tab[idx].querySelector<input>('.input-nx').value);
+    const ny = parseInt(tab[idx].querySelector<input>('.input-ny').value);
+    if (!nx) {
+      alertError('nx를 입력해주세요.');
+      return;
+    }
+    if (nx < 1 || 149 < nx) {
+      alertError('nx는 1 ~ 149 내의 숫자로 입력해주세요.');
+      return;
+    }
+    if (!ny) {
+      alertError('ny를 입력해주세요.');
+      return;
+    }
+    if (ny < 1 || 253 < ny) {
+      alertError('ny는 1 ~ 253 내의 숫자로 입력해주세요.');
+      return;
+    }
+    url = '/weather/getWeatherDay';
+    formdata = { idx: i, nx, ny };
+  } else {
+    if (!regNums[i]) {
+      alertError('지역을 선택해주세요.');
+      return;
+    }
+    url = `/weather/getWeather${i <= 6 ? 'Ta' : 'Ml'}`;
+    formdata = { idx: i, reg: regNums[i] };
+  }
+  
+  btnStart.disabled = true;
+  btnStart.classList.replace('btn-success', 'btn-warning');
+  btnStart.innerHTML = '<i class="fa-solid fa-hourglass"></i>';
+  
+  fetch(url, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(formdata),
+  })
+  .then((res) => res.json())
+  .then((data: number[]) => {
+    const userContainer = tab[idx].querySelector<div>('.user-container');
+    const anss = userContainer.querySelectorAll<input>('.item-answer');
+    const userList = userContainer.children;
+    const colors = ['#3c73ff', 'black', '#ff413b'];
+    for (let j = 1; j < userList.length - 1; j++) {
+      const inputs = userList[j].querySelectorAll<input>('.item-input');
+      const diffs = userList[j].querySelectorAll<span>('.item-result');
+      const score = userList[j].querySelector<span>('.item-result-total');
+      let total = 0;
+      for (let k = 0; k < 7; k++) {
+        if (inputs[k].value) {
+          const diff = parseInt(inputs[k].value) - data[k];
+          diffs[k].innerText = String(diff);
+          diffs[k].style.color = colors[+(diff > 0) - +(diff < 0) + 1];
+          total += diff;
+        }
+      }
+      score.innerText = String(total);
+      score.style.color = colors[+(total > 0) - +(total < 0) + 1];
+    }
+    for (let j = 0; j < 7; j++) {
+      anss[j].value = String(data[j]);
+    }
+  })
+  .catch((err) => {
+    console.error('오류인');
+    console.error(err);
+  })
+  .finally(() => {
+    btnStart.disabled = false;
+    btnStart.classList.replace('btn-warning', 'btn-success');
+    btnStart.innerText = '시작!';
+  });
+}
+
+// hide clone templates
+userContainer.forEach((container) => {
+  (<div>container.children[0]).style.display = 'none';
+});
