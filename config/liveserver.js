10c3b8f0c07b6b2e5af78179c1fbf37f2af30438
abcworld123 <rkaxhdals@gmail.com>
2022-04-27 18:09:40 +0900

    Fix live server

--- config/liveserver.js
+++ config/liveserver.js
@@ -1,13 +1,13 @@
-const livereload = require('livereload');
-const liveServer = livereload.createServer({ exts: ['ejs', 'css', 'js'] });
-
-liveServer.watch([
-  '/views',
-  '/src/css',
-  '/public/stylesheets',
-  '/public/javascripts',
-].map(path => __dirname + path));
-
 module.exports = (app) => {
+  const livereload = require('livereload');
+  const liveServer = livereload.createServer({ exts: ['ejs', 'css', 'js'] });
+
+  liveServer.watch([
+    '/views',
+    '/src/css',
+    '/public/stylesheets',
+    '/public/javascripts',
+  ].map(path => process.env.PWD + path));
+
   app.use(require('connect-livereload')());
 };
