1cd0ab23c90b59fa1d506b5ac24ef17096fd5b55
KimDeokjung <ejrwnd123@gmail.com>
2022-06-18 23:51:59 +0900

    tidy up socket code

--- sockets/games/playboard.js
+++ sockets/games/playboard.js
@@ -6,6 +6,8 @@ module.exports = function (nsp) {
   const heightPixel = 500;
 
   nsp.on('connection', (socket) => {
+    let p1, p2, ball, info;
+
     users.add(socket.id);
     socket.emit('user list', [...users]);
     socket.emit('room list', [...rooms]);
@@ -16,25 +18,25 @@ module.exports = function (nsp) {
     });
 
     socket.on('disconnect', (reason) => {
-      console.log("나감");
+      console.log('나감');
     });
 
     socket.on('moveUp', (roomNum) => {
       const userInfo = playBoard.get(roomNum);
-      if (userInfo.play1 === socket.id) {
-        userInfo.player1_dy = -userInfo.player1_y_speed;
+      if (userInfo.p1._id === socket.id) {
+        userInfo.p1.dy = -userInfo.p1.ySpeed;
       } else {
-        userInfo.player2_dy = -userInfo.player2_y_speed;
+        userInfo.p2.dy = -userInfo.p2.ySpeed;
       }
       playBoard.set(roomNum, userInfo);
     });
 
     socket.on('moveDown', (roomNum) => {
       const userInfo = playBoard.get(roomNum);
-      if (userInfo.play1 === socket.id) {
-        userInfo.player1_dy = userInfo.player1_y_speed;
+      if (userInfo.p1._id === socket.id) {
+        userInfo.p1.dy = userInfo.p1.ySpeed;
       } else {
-        userInfo.player2_dy = userInfo.player2_y_speed;
+        userInfo.p2.dy = userInfo.p2.ySpeed;
       }
       playBoard.set(roomNum, userInfo);
     });
@@ -43,80 +45,91 @@ module.exports = function (nsp) {
       if (!playBoard.has(roomNum)) {
         socket.join(roomNum);
         playBoard.set(roomNum, {
-          "play1": socket.id,
-          "play2": "",
-          "player1_x": 100,
-          "player1_y": 250,
-          "player1_dy": 5,
-          "player1_y_speed": 5,
-          "player1_score": 0,
-          "player2_x": 900,
-          "player2_y": 250,
-          "player2_dy": 5,
-          "player2_y_speed": 5,
-          "player2_score": 0,
-          "play_ball_x": 500,
-          "play_ball_y": 250,
-          "play_ball_dx": 5,
-          "play_ball_dy": 2,
-          "play_ball_pause": 0,
-          "time": 66,
-          "left_time": 50,
+          'p1': {
+            '_id': socket.id,
+            'x': 100,
+            'y': 250,
+            'dy': 5,
+            'ySpeed': 5,
+            'score': 0,
+          },
+          'p2': {
+            '_id': '',
+            'x': 900,
+            'y': 250,
+            'dy': 5,
+            'ySpeed': 5,
+            'score': 0,
+          },
+          'ball': {
+            'x': 500,
+            'y': 250,
+            'dx': 5,
+            'dy': 2,
+            'pause': 0,
+          },
+          'time': 66,
+          'left_time': 50,
         });
       } else {
         const userInfo = playBoard.get(roomNum);
-        if (!userInfo.play2) {
-          userInfo.play2 = socket.id;
+        if (!userInfo.p2._id) {
+          userInfo.p2._id = socket.id;
           socket.join(roomNum);
           playBoard.set(roomNum, userInfo);
 
-          abc(userInfo.play1, userInfo.play2, "countDown", roomNum);
+          abc(userInfo.p1._id, userInfo.p2._id, 'countDown', roomNum);
           setTimeout(function () {userInfo.gameBoard = setInterval(calPlay.bind(this, roomNum), 15 ); }, 3000);
         }
       }
     });
-    function abc(play1, play2, num, roomNum) {
-      nsp.to(roomNum).emit(num);
+
+    function abc(p1, p2, num, roomNum) {
+      console.log(p1);
+      console.log(p2);
+      console.log(socket.id);
+      nsp.to(p1).emit(num, 1);
+      nsp.to(p2).emit(num, 2);
     }
 
     function calPlay(roomNum) {
       let userInfo = playBoard.get(roomNum);
-      userInfo.player1_y += userInfo.player1_dy;
-      userInfo.player2_y += userInfo.player2_dy;
-      userInfo.play_ball_x += userInfo.play_ball_dx;
-      userInfo.play_ball_y += userInfo.play_ball_dy;
-      userInfo.play_ball_pause = userInfo.play_ball_pause < 1 ? userInfo.play_ball_pause : userInfo.play_ball_pause - 1;
+      userInfo.p1.y += userInfo.p1.dy;
+      userInfo.p2.y += userInfo.p2.dy;
+      userInfo.ball.x += userInfo.ball.dx;
+      userInfo.ball.y += userInfo.ball.dy;
+      userInfo.ball.pause = userInfo.ball.pause < 1 ? userInfo.ball.pause : userInfo.ball.pause - 1;
       userInfo.time = userInfo.time - 1;
 
-      if ( userInfo.player1_y >= heightPixel * 0.8 || userInfo.player1_y <= 0) {userInfo.player1_dy = 0; }
-      if ( userInfo.player2_y >= heightPixel * 0.8 || userInfo.player2_y <= 0) {userInfo.player2_dy = 0; }
-      if (userInfo.play_ball_x >= widthPixel && userInfo.play_ball_pause < 1) {
-        userInfo.play_ball_dx = -userInfo.play_ball_dx;
-        userInfo.player1_score = userInfo.player1_score + 1;
-        nsp.to(roomNum).emit('playboard', userInfo.player1_x, userInfo.player1_y, userInfo.player2_x, userInfo.player2_y, Math.round(userInfo.play_ball_x), Math.round(userInfo.play_ball_y), userInfo.player1_score, userInfo.player2_score);
+      if ( userInfo.p1.y >= heightPixel * 0.8 || userInfo.p1.y <= 0) {userInfo.p1.dy = 0; }
+      if ( userInfo.p2.y >= heightPixel * 0.8 || userInfo.p2.y <= 0) {userInfo.p2.dy = 0; }
+      if (userInfo.ball.x >= widthPixel && userInfo.ball.pause < 1) {
+        userInfo.ball.dx = -userInfo.ball.dx;
+        userInfo.p1.score = userInfo.p1.score + 1;
+        nsp.to(roomNum).emit('playboard', userInfo.p1.x, userInfo.p1.y, userInfo.p2.x, userInfo.p2.y, Math.round(userInfo.ball.x), Math.round(userInfo.ball.y), userInfo.p1.score, userInfo.p2.score);
         setInitialState(userInfo);
 
         nsp.to(roomNum).emit('player1_goal');
 
         setTimeout(function () {
-          nsp.to(roomNum).emit('playboard', userInfo.player1_x, userInfo.player1_y, userInfo.player2_x, userInfo.player2_y, Math.round(userInfo.play_ball_x), Math.round(userInfo.play_ball_y), userInfo.player1_score, userInfo.player2_score);
+          nsp.to(roomNum).emit('playboard', userInfo.p1.x, userInfo.p1.y, userInfo.p2.x, userInfo.p2.y, Math.round(userInfo.ball.x), Math.round(userInfo.ball.y), userInfo.p1.score, userInfo.p2.score);
           nsp.to(roomNum).emit('countDown');
         }, 3000);
 
         clearInterval(userInfo.gameBoard);
         gameLoading(userInfo, roomNum);
         return;
-      } else if (userInfo.play_ball_x <= 0 && userInfo.play_ball_pause < 1) {
-        userInfo.play_ball_dx = -userInfo.play_ball_dx;
-        userInfo.player2_score = userInfo.player2_score + 1;
-        nsp.to(roomNum).emit('playboard', userInfo.player1_x, userInfo.player1_y, userInfo.player2_x, userInfo.player2_y, Math.round(userInfo.play_ball_x), Math.round(userInfo.play_ball_y), userInfo.player1_score, userInfo.player2_score);
+      } else if (userInfo.ball.x <= 0 && userInfo.ball.pause < 1) {
+        userInfo.ball.dx = -userInfo.ball.dx;
+        userInfo.p2.score = userInfo.p2.score + 1;
+        nsp.to(roomNum).emit('playboard', userInfo.p1.x, userInfo.p1.y, userInfo.p2.x, userInfo.p2.y, Math.round(userInfo.ball.x), Math.round(userInfo.ball.y), userInfo.p1.score, userInfo.p2.score);
 
         setInitialState(userInfo);
 
         nsp.to(roomNum).emit('player2_goal');
 
         setTimeout(function () {
-          nsp.to(roomNum).emit('playboard', userInfo.player1_x, userInfo.player1_y, userInfo.player2_x, userInfo.player2_y, Math.round(userInfo.play_ball_x), Math.round(userInfo.play_ball_y), userInfo.player1_score, userInfo.player2_score);
+          nsp.to(roomNum).emit('playboard', userInfo.p1.x, userInfo.p1.y, userInfo.p2.x, userInfo.p2.y, Math.round(userInfo.ball.x), Math.round(userInfo.ball.y), userInfo.p1.score, userInfo.p2.score);
           nsp.to(roomNum).emit('countDown');
         }, 3000);
 
@@ -124,15 +137,15 @@ module.exports = function (nsp) {
         gameLoading(userInfo, roomNum);
         return;
       }
-      if ( userInfo.play_ball_y >= heightPixel || userInfo.play_ball_y <= 0) {
-        userInfo.play_ball_dy = -userInfo.play_ball_dy;
+      if ( userInfo.ball.y >= heightPixel || userInfo.ball.y <= 0) {
+        userInfo.ball.dy = -userInfo.ball.dy;
       }
       if (userInfo.time < 1) {
         nsp.to(roomNum).emit('timeFlow');
         if (userInfo.left_time < 2) {
           clearInterval(userInfo.gameBoard);
         }
-        userInfo.play_ball_dx = userInfo.play_ball_dx > 0 ? userInfo.play_ball_dx + 1 : userInfo.play_ball_dx - 1;
+        userInfo.ball.dx = userInfo.ball.dx > 0 ? userInfo.ball.dx + 1 : userInfo.ball.dx - 1;
         userInfo.left_time -= 1;
         userInfo.time = 66;
       }
@@ -141,45 +154,49 @@ module.exports = function (nsp) {
 
       userInfo = checkCrash(userInfo);
 
-      nsp.to(roomNum).emit('playboard', userInfo.player1_x, userInfo.player1_y, userInfo.player2_x, userInfo.player2_y, Math.round(userInfo.play_ball_x), Math.round(userInfo.play_ball_y), userInfo.player1_score, userInfo.player2_score);
+      nsp.to(roomNum).emit('playboard', userInfo.p1.x, userInfo.p1.y, userInfo.p2.x, userInfo.p2.y, Math.round(userInfo.ball.x), Math.round(userInfo.ball.y), userInfo.p1.score, userInfo.p2.score);
     }
 
     function checkCrash(userInfo) {
-      if (userInfo.player1_x < userInfo.play_ball_x &&
-        userInfo.play_ball_x < userInfo.player1_x + 20 &&
-        userInfo.player1_y < userInfo.play_ball_y &&
-        userInfo.play_ball_y < userInfo.player1_y + heightPixel * 0.2) {
-        userInfo.play_ball_dx *= -1;
+      if (userInfo.p1.x < userInfo.ball.x &&
+        userInfo.ball.x < userInfo.p1.x + 20 &&
+        userInfo.p1.y < userInfo.ball.y &&
+        userInfo.ball.y < userInfo.p1.y + heightPixel * 0.2) {
+        userInfo.ball.dx *= -1;
         let dy = Math.floor(Math.random() * 200);
-        dy = userInfo.player1_dy < 0 ? -dy : dy;
+        dy = userInfo.p1_dy < 0 ? -dy : dy;
         dy /= 20;
-        userInfo.play_ball_dy = dy;
-        userInfo.play_ball_pause = 5;
-      } else if (userInfo.player2_x < userInfo.play_ball_x &&
-        userInfo.play_ball_x < userInfo.player2_x + 20 &&
-        userInfo.player2_y < userInfo.play_ball_y &&
-        userInfo.play_ball_y < userInfo.player2_y + heightPixel * 0.2) {
-        userInfo.play_ball_dx *= -1;
+        userInfo.ball.dy = dy;
+        userInfo.ball.pause = 5;
+      } else if (userInfo.p2.x < userInfo.ball.x &&
+        userInfo.ball.x < userInfo.p2.x + 20 &&
+        userInfo.p2.y < userInfo.ball.y &&
+        userInfo.ball.y < userInfo.p2.y + heightPixel * 0.2) {
+        userInfo.ball.dx *= -1;
         let dy = Math.floor(Math.random() * 400);
         dy -= 200;
-        dy = userInfo.player2_dy < 0 ? -dy : dy;
+        dy = userInfo.p2.dy < 0 ? -dy : dy;
         dy /= 20;
-        userInfo.play_ball_dy = dy;
-        userInfo.play_ball_pause = 5;
+        userInfo.ball.dy = dy;
+        userInfo.ball.pause = 5;
       }
       return userInfo;
     }
 
     function setInitialState(userInfo) {
-      userInfo.player1_y = 240;
-      userInfo.player2_y = 240;
-      userInfo.play_ball_x = 500;
-      userInfo.play_ball_y = 250;
-      userInfo.play_ball_dx = 5;
+      userInfo.p1.y = 240;
+      userInfo.p2.y = 240;
+      userInfo.ball.x = 500;
+      userInfo.ball.y = 250;
+      userInfo.ball.dx = 5;
     }
 
     function gameLoading(userInfo, roomNum) {
       setTimeout(function () {userInfo.gameBoard = setInterval(calPlay.bind(this, roomNum), 15 ); }, 6000);
+    }
+
+    function setGameInfo() {
+
     }
   });
 };
