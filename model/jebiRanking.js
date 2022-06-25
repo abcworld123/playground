47c6f25d52d92225085b25d45c56d37f9bcb8e08
abcworld123 <rkaxhdals@gmail.com>
2022-06-25 09:35:59 +0900

    Fix mongoose model

--- model/jebiRanking.js
+++ model/jebiRanking.js
@@ -1,7 +1,11 @@
-const mongoose = require('mongoose');
-const Ranking = mongoose.model('Ranking', {
-  n: Number,
-  count: [Number],
-  total: Number,
+const { Schema, model } = require('mongoose');
+
+const rankingSchema = new Schema({
+  n: { type: Number, required: true },
+  count: { type: [Number], required: true },
+  total: { type: Number, required: true },
 });
+
+const Ranking = model('Ranking', rankingSchema);
+
 module.exports = Ranking;
